94fafdbabf315e663f57f6d5323e4ee6871186ce
Pauline <gokowanjiru@gmail.com>
2023-02-09 07:35:14 +0300

    added functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import javax.swing.text.html.InlineView
+
+fun main(){
+    greeting("Pauline")
+}
+
+fun greeting(name : String){
+    println("Hello " + name)
+
+}
+
+fun division (num1: Int, num2: Int): Int{
+    var division = ( num1 / num2)
+    return division
+
+}
+
+fun add (num2: Int, num3: Int, num4: Int, num5: Int): Int{
+    var add = ( num2 + num3 + num4 + num5)
+    return add
+}
+fun print(){
+    var text = ("i have a good sense of humour")
+    println("text")
+
+
+}
+
+fun fact() {
+    var fact = "I have a good sene of humour"
+    println(fact)
+}
